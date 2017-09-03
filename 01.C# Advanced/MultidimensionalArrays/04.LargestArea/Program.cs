301115fb8007a9106b8cdc77fb698121bed9b988
Kolchakov <nikola.kolchakov@gmail.com>
2017-09-03 21:23:53 +0300

    better solution

--- 01.C# Advanced/MultidimensionalArrays/04.LargestArea/Program.cs
+++ 01.C# Advanced/MultidimensionalArrays/04.LargestArea/Program.cs
@@ -15,34 +15,22 @@ namespace _04.LargestArea
 4 3 1 3 3 1
 4 3 3 3 1 1
 
-3 3 
-1 1 1 
-1 1 1 
-1 1 1  
+3 1
+1
+1
+1
      */
     class Program
     {
-        class Node
-        {
-            public Node(int data, int r, int c)
-            {
-                this.Data = data;
-                this.Row = r;
-                this.Col = c;
-            }
-
-            public int Data { get; set; }
-            public int Row { get; set; }
-            public int Col { get; set; }
-        }
+        static int[,] mtx;
+        static bool[,] visited;
         static void Main(string[] args)
         {
-            var size = Console.ReadLine().Split(' ');
-            int n = int.Parse(size[0]);
-            int m = int.Parse(size[1]);
+            int[] size = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int n = size[0];
+            int m = size[1];
 
-            int[,] mtx = new int[n, m];
-
+            mtx = new int[n, m];
             for (int i = 0; i < mtx.GetLength(0); i++)
             {
                 var line = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
@@ -52,94 +40,58 @@ namespace _04.LargestArea
                 }
             }
 
-            bool[,] visited = new bool[n, m];
-            bool[,] markedAsChild = new bool[n, m];
+            visited = new bool[n, m];
 
-            List<int> maxCount = new List<int>();
-
+            int max = int.MinValue;
             for (int r = 0; r < mtx.GetLength(0); r++)
             {
                 for (int c = 0; c < mtx.GetLength(1); c++)
                 {
-                    //if (visited[r, c])
-                    //{
-                    //    continue;
-                    //}
-                    var stack = new Stack<Node>();
-                    stack.Push(new Node(mtx[r, c], r, c));
-
-                    int count = 0;
-                    while (stack.Count != 0)
+                    if (visited[r, c])
                     {
-                        var currNode = stack.Pop();
-                        count++;
-                        if (!visited[currNode.Row, currNode.Col])
-                        {
-                            visited[currNode.Row, currNode.Col] = true;
-                            IEnumerable<Node> neighbours = GetNodeNeighbours(mtx, currNode, visited, markedAsChild);
-
-                            foreach (var neigh in neighbours)
-                            {
-                                stack.Push(neigh);
-                            }
-                        }
+                        continue;
                     }
-                    maxCount.Add(count);
-
+                    int curr = RecSearch(r, c);
+                    if (max < curr)
+                    {
+                        max = curr;
+                    }
                 }
             }
-
-            Console.WriteLine(maxCount.Max() - 1);
+            Console.WriteLine(max);
         }
 
-        private static IEnumerable<Node> GetNodeNeighbours(int[,] mtx, Node currNode, bool[,] visited, bool[,] marked)
+        static int RecSearch(int r, int c)
         {
-            List<Node> res = new List<Node>();
-            int r = currNode.Row;
-            int c = currNode.Col;
-
-            // left
-            if (c - 1 >= 0)
+            if (visited[r, c])
             {
-                if (mtx[r, c - 1] == currNode.Data && !marked[r, c - 1])
-                {
-                    res.Add(new Node(mtx[r, c - 1], r, c - 1));
-                    marked[r, c - 1] = true;
-                }
+                return 0;
             }
 
-            // top
-            if (r - 1 >= 0)
-            {
-                if (mtx[r - 1, c] == currNode.Data && !marked[r - 1, c])
-                {
-                    res.Add(new Node(mtx[r - 1, c], r - 1, c));
-                    marked[r - 1, c] = true;
-                }
-            }
+            int childrenCount = 0;
 
-            // right
-            if (c + 1 < mtx.GetLength(1))
-            {
-                if (mtx[r, c + 1] == currNode.Data && !marked[r, c + 1])
-                {
-                    res.Add(new Node(mtx[r, c + 1], r, c + 1));
-                    marked[r, c + 1] = true;
-                }
-            }
+            visited[r, c] = true;
 
-            // bottom
-            if (r + 1 < mtx.GetLength(0))
+            if (c - 1 >= 0 && !visited[r, c - 1] && mtx[r, c - 1] == mtx[r, c]) // check left
             {
-                if (mtx[r + 1, c] == currNode.Data && !marked[r + 1, c])
-                {
-                    res.Add(new Node(mtx[r + 1, c], r + 1, c));
-                    marked[r + 1, c] = true;
-                }
+                childrenCount += RecSearch(r, c - 1);
+            }
+            if (r + 1 < mtx.GetLength(0) && !visited[r + 1, c] && mtx[r + 1, c] == mtx[r, c]) // down
+            {
+                childrenCount += RecSearch(r + 1, c);
 
             }
+            if (c + 1 < mtx.GetLength(1) && !visited[r, c + 1] && mtx[r, c + 1] == mtx[r, c]) // right
+            {
+                childrenCount += RecSearch(r, c + 1);
 
-            return res;
+            }
+            if (r - 1 >= 0 && !visited[r - 1, c] && mtx[r - 1, c] == mtx[r, c]) // top
+            {
+                childrenCount += RecSearch(r - 1, c);
+            }
+            return childrenCount + 1;
         }
+
     }
 }
