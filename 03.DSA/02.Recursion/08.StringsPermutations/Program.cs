d1a5045a28d606a5a45e55ecfd3f01ff31f7cc35
Kolchakov <nikola.kolchakov@gmail.com>
2017-09-23 18:02:04 +0300

    string permutation

---
+++ 03.DSA/02.Recursion/08.StringsPermutations/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.StringsPermutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Permutate(s);
+
+        }
+
+        private static void Permutate(string text)
+        {
+            Rec(text, "");
+        }
+
+        private static void Rec(string text, string result)
+        {
+            if (text.Length == 0)
+            {
+                Console.WriteLine(result);
+                return;
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char chosen = text[i];
+
+                result += chosen;
+                text = text.Remove(i, 1);
+
+                Rec(text, result);
+
+                text += chosen;
+                result = result.Remove(result.Length - 1, 1);
+            }
+        }
+    }
+}
